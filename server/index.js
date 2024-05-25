7c8a293fa47a3e4a6859c4a2e72ef4a2e268d61c
sakshammaggu <sakshammaggu80@gmail.com>
2024-05-25 15:42:02 +0530

    connecting to mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Mongodb connected successfully");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 
 const app=express();
 
 app.listen((3000), ()=>{
     console.log("Server is running at port 3000!");
 })
+
