ce364b3e6842df1da93b7b7b78e01d6011687291
sakshammaggu <sakshammaggu80@gmail.com>
2024-06-05 13:44:28 +0530

    showing user data on header part when the user is signed in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,51 +1,90 @@
 import React from 'react'
 
-import { Navbar} from "flowbite-react";
+import { Avatar, Dropdown, Navbar} from "flowbite-react";
 import { Link } from 'react-router-dom';
 import { Button } from 'flowbite-react';
+import {useSelector} from 'react-redux';
 
 export default function Header() {
-  return (
-    <Navbar className='border-b-2'>
+    const {currentUser}=useSelector(state=>state.user);
 
-        {/* Logo */}
-        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Virtu</span>
-            Meet
-        </Link>
+    return (
+        <Navbar className='border-b-2'>
 
-        {/* Links to other pages on large window size*/}
-        <div className="flex gap-2 md:order-2">
-            <Link to="/sign-in">
-                <Button gradientDuoTone='greenToBlue'>
-                    Sign In
-                </Button>
+            {/* Logo */}
+            <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Virtu</span>
+                Meet
             </Link>
 
-            <Link to="/sign-up">
-                <Button gradientDuoTone='redToYellow'>
-                    Sign Up
-                </Button>
-            </Link>
+            {/* Links to other pages on large window size*/}
+            <div className="flex gap-2 md:order-2">
+                { 
+                    currentUser ? (
+                        <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar 
+                                    alt='user'
+                                    img={currentUser.profilePicture}
+                                    rounded
+                                />
 
-            <Navbar.Toggle />
-        </div>
+                                
+                            }
+                        >
+                        
+                            <Dropdown.Header>
+                                <span className='block text-sm'>@{currentUser.userName}</span>
+                                <span className='block text-sm font-medium'>{currentUser.email}</span>
+                            </Dropdown.Header>
 
-        {/* Links to other pages on mobile window size*/}
-        <Navbar.Collapse>
-            <Navbar.Link>
-                <Link to='/'>Home</Link>
-            </Navbar.Link>
+                            <Link to='/profile'>
+                                <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
 
-            <Navbar.Link>
-                <Link to='/about'>About</Link>
-            </Navbar.Link>
+                            <Dropdown.Divider />
+                            <Dropdown.Item>Sign Out</Dropdown.Item>
+
+                        </Dropdown>
+
+                    ) : 
+                    (
+                        <>
+                            <Link to="/sign-in">
+                                <Button gradientDuoTone='greenToBlue'>
+                                    Sign In
+                                </Button>
+                            </Link>
+
+                            <Link to="/sign-up">
+                                <Button gradientDuoTone='redToYellow'>
+                                    Sign Up
+                                </Button>
+                            </Link>
+                        </>
+                    )
+                }
+
+                <Navbar.Toggle />
+            </div>
+
+            {/* Links to other pages on mobile window size*/}
+            <Navbar.Collapse>
+                <Navbar.Link>
+                    <Link to='/'>Home</Link>
+                </Navbar.Link>
+
+                <Navbar.Link>
+                    <Link to='/about'>About</Link>
+                </Navbar.Link>
+                    
+                <Navbar.Link>
+                    <Link to='/meeting'>Meeting</Link>
+                </Navbar.Link>
                 
-            <Navbar.Link>
-                <Link to='/meeting'>Meeting</Link>
-            </Navbar.Link>
-            
-        </Navbar.Collapse>
-    </Navbar>
-  )
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
