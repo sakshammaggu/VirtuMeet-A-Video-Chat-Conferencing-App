6d8f53ed79c33cb065edfab3f1db8c8431e5700a
sakshammaggu <sakshammaggu80@gmail.com>
2024-06-11 12:04:58 +0530

    created meeting modal component

---
+++ client/src/components/Meetings/MeetingModal.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Label, Modal, TextInput } from 'flowbite-react';
+
+export default function MeetingModal({openModal, toggleModal}) {
+    const [meetingUsername, setMeetingUsername] = useState('');
+    const cleanUpInput = (input) => {
+        return input.replace(/\s+/g, ' ').trim();
+      };
+    const handleMeetingFormChange=(e)=>{
+        const cleanedInput = cleanUpInput(e.target.value);
+        setMeetingUsername(cleanedInput);
+    };
+    useEffect(() => {
+        if (!openModal){
+            setMeetingUsername('');
+        }
+    }, [openModal])
+
+    return (
+        <Modal show={openModal} onClose={toggleModal} size="lg">
+            <Modal.Header>Host A New Meeting</Modal.Header>
+
+            <Modal.Body>
+                <div className="space-y-6 -mt-4">
+                    <p className="font-semibold text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed text-gray-500 dark:text-gray-400">
+                        Start an instant meeting.
+                    </p>
+                </div>
+                <br />
+                <form onChange={handleMeetingFormChange}>
+                    <div className="mb-2 block">
+                        <Label htmlFor="meetingUsername" value="Set Your Meeting Username" />
+                    </div>
+                    <TextInput type="text" id="meetingUsername" required />
+                </form>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button className="w-full sm:w-auto" onClick={toggleModal}>Host Meeting</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
--- client/src/components/Meetings/Tabs/Home.jsx
+++ client/src/components/Meetings/Tabs/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from "flowbite-react";
 
 import { BiSolidVideoPlus } from "react-icons/bi";
 import { MdMeetingRoom } from "react-icons/md";
 import { RiCalendarScheduleFill } from "react-icons/ri";
 import { BiSolidVideoRecording } from "react-icons/bi";
+import MeetingModal from '../MeetingModal';
 
 export default function Home() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const toggleModal=()=>{ 
+    setIsOpenModal(!isOpenModal);
+  };
+
   return (
     <div className='min-h-screen grid grid-cols-1 sm:grid-cols-2 gap-10 lg:p-24 lg:mx-24 sm:p-16 sm:-mt-10 mx-4 sm:mx-7 pb-16'>
-      <Card className="max-w-sm cursor-pointer hover:bg-gradient-to-r from-red-400 to-yellow-200 text-center transform transition-transform duration-300 hover:scale-105">
-        <BiSolidVideoPlus className="text-gray-700 text-4xl -mt-5"/>
+      <Card onClick={toggleModal} className="max-w-sm cursor-pointer hover:bg-gradient-to-r from-red-400 to-yellow-200 text-center transform transition-transform duration-300 hover:scale-105">
+        <BiSolidVideoPlus className="text-gray-700 text-4xl -mt-5 mx-20"/>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           New Meeting
         </h5>
@@ -19,8 +25,12 @@ export default function Home() {
         </p>
       </Card>
 
+      {
+        toggleModal && <MeetingModal openModal={isOpenModal} toggleModal={toggleModal} />
+      }
+
       <Card className="max-w-sm cursor-pointer hover:bg-gradient-to-r from-blue-400 to-green-300 text-center transform transition-transform duration-300 hover:scale-105">
-        <MdMeetingRoom className="text-gray-700 text-4xl -mt-5"/>
+        <MdMeetingRoom className="text-gray-700 text-4xl -mt-5 mx-20"/>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Join Meeting
         </h5>
@@ -30,7 +40,7 @@ export default function Home() {
       </Card>
 
       <Card className="max-w-sm cursor-pointer hover:bg-gradient-to-r from-lime-400 to-cyan-300 text-center transform transition-transform duration-300 hover:scale-105">
-        <RiCalendarScheduleFill className="text-gray-700 text-4xl -mt-5"/>
+        <RiCalendarScheduleFill className="text-gray-700 text-4xl -mt-5 mx-20"/>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Schedule Meeting
         </h5>
